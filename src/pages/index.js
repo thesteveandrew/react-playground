28b5bfc2c6b819e31e1ae8957137e53c99d3cf32
Stephen Murrill <s.murrill@slangapp.com>
2021-10-12 09:56:54 -0400

    styled new header

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,16 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Watch video</Link> <br />
-    </p>
-  </Layout>
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+
+  </div>
 )
 
 export default IndexPage
